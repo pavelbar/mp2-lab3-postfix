d5ba5279ba3983b78f4526a415c2db1558be5b59
pavelbar <pavelbaranow@gmail.com>
2016-10-26 01:41:13 +0400

    Класс стек + тесты на него

--- Баранов/base/stack.h
+++ Баранов/base/stack.h
@@ -1,11 +1,59 @@
 #ifndef __STACK_H__
 #define __STACK_H__
-
+const int MaxSize = 100;
 template <class T>
 class TStack
 {
-  T *pMem;
+protected:
+	T *pMem;
+	int size;
+	int count;
 public:
+	TStack(int len);
+	~TStack();
+	bool isEmpty();
+	bool isFull();
+	void Push(const T &val);//add
+	T Pop();//взять верхний и удалить
+	T Top();//view
 };
+//-----------------------------------
+template <class ValType>
+TStack<T>::TStack(int len) {
+	if ((len<1) || (len>MaxSize)) throw (len);
+	size = len;
+	count = 0;
+	pMem = new T[size];
+}
 
+template <class T>
+TStack<T>::~TStack() {
+	delete[]pMem;
+	pMem = NULL;
+}
+
+template <class T>
+bool TStack<T>::IsEmpty() {
+	return(count == MaxSize);
+}
+
+template <class T>
+bool TStack<T>::isFull() {
+	return (count == 0);
+}
+
+template <class T>
+void TStack<T>::Push(const T &val) {
+	if (isFull())  throw (count);
+	return pMem[count++] = val; }
+
+template <class T>
+T TStack<T>::Pop() {
+	if (IsEmpty) throw (count);
+	return pMem[(count--) - 1]; }
+
+template <class T>
+T TStack<T>::Top() {
+	if (IsEmpty) throw (count);
+	return pMem[count - 1]; }
 #endif
--- Баранов/base_test/test_tstack.cpp
+++ Баранов/base_test/test_tstack.cpp
@@ -1,7 +1,87 @@
 #include "stack.h"
 #include <gtest.h>
 
-TEST(TStack, can_create_stack_with_positive_length)
+TEST(TStack, can_create_stack_with_positive_length)//стек с положительной длиной
 {
-  ADD_FAILURE();
+	ASSERT_NO_THROW(TStack<int> v(5));
+}
+
+TEST(TStack, throws_when_create_vector_with_negative_length)//стек с отрицательной длиной
+{
+	ASSERT_ANY_THROW(TStack<int> v(-5));
+}
+
+TEST(TStack, can_create_vector_with_equal_length) //стек с нулевой длиной
+{
+	ASSERT_ANY_THROW(TStack<int> v(0));
+}
+
+TEST(TStack, cant_create_too_large_vector) //стек большой длины 
+{
+	ASSERT_ANY_THROW(TStack<int> v(MaxSize + 1));
+}
+
+TEST(TStack, Push_and_full_stack) //добавить в полный стек
+{
+	TStack<int> v(2);
+	v.Push(4);
+	v.Push(4);
+	ASSERT_ANY_THROW(v.Push(4));
+}
+
+TEST(TStack, Pop_and_clear_stack) //показать и удалить из пустого стека
+{
+	TStack<int> v(2);
+	ASSERT_ANY_THROW(v.Pop());
+}
+
+TEST(TStack, Top_and_clear_stack) //показать из пустого стека
+{
+	TStack<int> v(2);
+	ASSERT_ANY_THROW(v.Top());
+}
+
+
+TEST(TStack, Push_and_Top)//добавить в стек и показать
+{
+	TStack<int> v(5);
+	v.Push(4);
+	EXPECT_EQ(4, v.Top());
+}
+
+TEST(TStack, Push_and_Pop) //добавить в стек и показать, показать и удалить, показать
+{
+	TStack<int> v(5);
+	v.Push(4);
+	v.Push(5);
+	EXPECT_EQ(5, v.Pop());
+	EXPECT_EQ(4, v.Top());
+}
+
+TEST(TStack, isEmpty_false) //полный стек - не пуст
+{
+	TStack<int> v(2);
+	v.Push(4);
+	v.Push(4);
+	EXPECT_EQ(0, IsEmpty());
+}
+
+TEST(TStack, isEmpty_trye) //пустой стек - пуст
+{
+	TStack<int> v(2);
+	EXPECT_EQ(1, IsEmpty());
+}
+
+TEST(TStack, isFull_try) //полный стек - полон
+{
+	TStack<int> v(2);
+	v.Push(4);
+	v.Push(4);
+	EXPECT_EQ(1, isFull());
+}
+
+TEST(TStack, isFull_false)  //пустой стек - не полон
+{
+	TStack<int> v(2);
+	EXPECT_EQ(0, isFull());
 }
