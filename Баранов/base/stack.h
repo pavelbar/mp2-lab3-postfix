e41348b4062acd0e85c07e592da4bf371c69e566
pavelbar <pavelbaranow@gmail.com>
2016-10-31 14:00:11 +0300

    correct

--- Баранов/base/stack.h
+++ Баранов/base/stack.h
@@ -1,27 +1,66 @@
 ﻿#ifndef __STACK_H__
 #define __STACK_H__
+#include <cstddef>
 
-const int MaxStackSize = 100;
-
+const int MaxSize = 100;
 template <class T>
 class TStack
 {
-  T *pMem;
-  int size;
-  int top;
+protected:
+	T *pMem;
+	int size;
+	int count;
 public:
-  TStack(int _size)
-  {
-    size = _size;
-    top = -1;
-    if ((size < 1) || (size > MaxStackSize))
-      throw size;
-    pMem = new T[size];
-  }
-  ~TStack()
-  {
-    delete [] pMem;
-  }
+	TStack(int = 10);
+	~TStack();
+	bool IsEmpty();
+	bool isFull();
+	void Push(const T &val);//add
+	T Pop();//âçÿòü âåðõíèé è óäàëèòü
+	T Top();//view
 };
+//-----------------------------------
+template <class T>
+TStack<T>::TStack(int len) {
+	if ((len<1) || (len>MaxSize)) throw (len);
+	size = len;
+	count = 0;
+	pMem = new T[size];//2 ýë-òà pMem[0] and pMem[1]
+}
 
+template <class T>
+TStack<T>::~TStack() {
+	delete[]pMem;
+	pMem = NULL;
+}
+
+template <class T>
+bool TStack<T>::IsEmpty() {
+	return(count == 0);
+}
+
+template <class T>
+bool TStack<T>::isFull() {
+	return (count == size);
+}
+
+template <class T>
+void TStack<T>::Push(const T &val) {
+	if (isFull())  throw (count);
+	pMem[count] = val;
+	count++;
+}
+
+template <class T>
+T TStack<T>::Pop() {
+	if (IsEmpty()) throw (count);
+	count--;
+	return pMem[count];
+}
+
+template <class T>
+T TStack<T>::Top() {
+	if (IsEmpty()) throw (count);
+	return pMem[count - 1];
+}
 #endif
