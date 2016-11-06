3b59fad31323e93e47b62fc722df68d11c8ddcd5
pavelbar <pavelbaranow@gmail.com>
2016-11-06 23:05:16 +0300

    исправил тесты на стек, написал калькулятор + тесты на него

--- Баранов/base/postfix.cpp
+++ Баранов/base/postfix.cpp
@@ -1,6 +1,8 @@
 ﻿#include "postfix.h"
 #include "stack.h"
 #include <iostream>
+#include <cctype>   // для функции isdigit
+#include <cstdlib>  // для функции atoi (преобразовать символы к типу int)
 using namespace std;
 
 string TPostfix::ToPostfix()
@@ -50,7 +52,7 @@ string TPostfix::ToPostfix()
             //--a--
             if (stack_op.IsEmpty() || stack_op.Top() == '(') stack_op.Push(infix[i]);
             //--b--
-            else if ( Priority(infix[i]) > Priority(stack_op.Top()) ) stack_op.Push(infix[i]);
+            else if (Priority(infix[i]) > Priority(stack_op.Top())) stack_op.Push(infix[i]);
             //--c--
             else
             {
@@ -61,7 +63,7 @@ string TPostfix::ToPostfix()
                     result[result_pos] = '\0';
                 }
                 stack_op.Push(infix[i]);
-            //-----
+                //-----
             }
         }
 
@@ -80,6 +82,53 @@ string TPostfix::ToPostfix()
 
 double TPostfix::Calculate()
 {
-    //not implemented
-    return 0;
+    //https://ru.wikipedia.org/wiki/%D0%9E%D0%B1%D1%80%D0%B0%D1%82%D0%BD%D0%B0%D1%8F_%D0%BF%D0%BE%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F_%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D1%8C#.D0.9E.D0.B1.D1.89.D0.B8.D0.B9_.D0.BF.D0.BE.D1.80.D1.8F.D0.B4.D0.BE.D0.BA  
+    //Алгоритм:
+    //1. Обработка входного символа
+    //    a) Если на вход подан операнд, он помещается на вершину стека.
+    //    b) Если на вход подан знак операции, то соответствующая операция выполняется над требуемым количеством значений, 
+    //    извлечённых из стека, взятых в порядке добавления.Результат выполненной операции кладётся на вершину стека.
+    //2. Если входной набор символов обработан не полностью, перейти к шагу 1.
+    //3. После полной обработки входного набора символов результат вычисления выражения лежит на вершине стека.
+    TStack<double> stack(MaxSize);
+    for (unsigned int i = 0; i < postfix.length(); i++)
+    {
+        //--a--
+        if (postfix[i] != '+' && postfix[i] != '-' && postfix[i] != '*' && postfix[i] != '/')
+        {
+            char tmp[MaxSize];
+            tmp[0] = postfix[i];
+            stack.Push(atoi(tmp));
+        }
+        else
+            //--b--
+        {
+            if (postfix[i] == '+')
+            {
+                double tmp1 = stack.Pop();
+                double tmp11 = stack.Pop();
+                stack.Push(tmp11 + tmp1);
+            }
+            if (postfix[i] == '-')
+            {
+                double tmp1 = stack.Pop();
+                double tmp11 = stack.Pop();
+                stack.Push(tmp11 - tmp1);
+            }
+            if (postfix[i] == '/')
+            {
+                double tmp1 = stack.Pop();
+                double tmp11 = stack.Pop();
+                stack.Push(tmp11 / tmp1);
+            }
+            if (postfix[i] == '*')
+            {
+                double tmp1 = stack.Pop();
+                double tmp11 = stack.Pop();
+                stack.Push(tmp11 * tmp1);
+            }
+        }
+
+    }
+    return stack.Pop();;
 }
--- Баранов/base_test/test_main.cpp
+++ Баранов/base_test/test_main.cpp
@@ -7,16 +7,6 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    //--DEBUG SEGMENT--
-    //TStack <int> v(5);
-    //v.Push(4);
-    //v.Push(7);
-    //cout << "Pop" << v.Pop()<<endl;         
-    //cout << "Top" <<  v.Top()<<endl;        
-    //cout << "IsEmpty  =" << v.IsEmpty()<<endl;
-    //cout << "IsFull  =" << v.IsFull()<<endl;
-    //-----------------
-    system("pause");
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
